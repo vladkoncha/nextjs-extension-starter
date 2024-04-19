81b9246fc8e3f3e8ec0f657a3e4fe7beec51920c
Vlad <vla.koncha@gmail.com>
2024-04-19 11:48:31 +0300

    patch hydration errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+
 import styles from './page.module.css';
 
 export default function Home() {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    return null; // return this null to avoid hydration errors
+  }
+
   return <main className={styles.main}>NextJS extension starter</main>;
 }
